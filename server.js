c3a636a6e8214d1ec6eaaae5290d0a301c4d0b67
M <m-hammed.bakkouch@student.ecp.fr>
2018-07-31 03:58:40 +0200

    first change

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const knex = require('knex');
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
-const image = require('./controllers/image');
 
 const db = knex({
   client: 'mysql',
@@ -24,7 +23,7 @@ const app = express();
 app.use(cors())
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=> { res.send(database.users) })
+app.get('/', (req, res)=> { res.send("it is working") })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/signup', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
