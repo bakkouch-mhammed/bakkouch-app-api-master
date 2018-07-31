7d2932eb52c678dc951cbf56d2b2b51047b8dd83
M <m-hammed.bakkouch@student.ecp.fr>
2018-07-31 03:33:23 +0200

    first change

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+  client: 'mysql',
+  connection: {
+    host : 'bakkoucheg123.mysql.db',
+    user : 'bakkoucheg123',
+    password : 'Bmhammed1995',
+    database : 'bakkoucheg123'
+  }
+});
+
+const app = express();
+
+app.use(cors())
+app.use(bodyParser.json());
+
+app.get('/', (req, res)=> { res.send(database.users) })
+app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/signup', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+
+app.listen(process.env.PORT || 8080 || 3000, ()=> {
+  console.log('app is running on port ${process.env.PORT}');
+})
